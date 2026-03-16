fc641a520619e595e5df60beefa5211e905de984
fcy <3181088318@qq.com>
2026-03-16 21:26:55 +0800

    add 新增了算法模块,更新了力扣hot100

---
+++ algorithm/leetcode/160test/main.go
@@ -0,0 +1,29 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+//func getIntersectionNode(headA, headB *ListNode) *ListNode {
+//	pA := headA
+//	pB := headB
+//	for pA != pB{
+//
+//		if pA == nil{
+//			// 为空就是读完了
+//			pA = headB
+//		}else{
+//			pA = pA.Next
+//		}
+//
+//		if pB == nil{
+//			pB = headA
+//		}else{
+//			pB = pB.Next
+//		}
+//	}
+//	return pA
+//}
